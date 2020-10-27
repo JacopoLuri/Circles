55c5dc01498fc954bf299f74f4301b63b0de9558
JacopoLuri <geomjacopoluri@gmail.com>
2020-10-27 13:36:23 +0100

    animation video not working

--- shape1/single/animated/sketch.js
+++ shape1/single/animated/sketch.js
@@ -1,12 +1,40 @@
 let time = 0;
+// let framerate = 60;
+// var capturer = new CCapture({ format: 'webm', name: 'shape1-single-animated', quality: 1, });
+// var capturer = new CCapture({
+//   format: 'gif',
+//   framerate,
+//   verbose: true
+// });
+
+// var capturer = new CCapture({ format: 'gif', workersPath: 'gif.worker.js' });
+
+// let capturer = new CCapture({
+//   framerate: 60,
+//   format: 'gif',
+//   workersPath: '/js/*',
+//   verbose: true,
+//   name: 'shape1-single-animated',
+// });
+
+// let gifLength = 180;
+let canvas;
 
 function setup() {
-  createCanvas(600, 600);
+  let p5canvas = createCanvas(600, 600);
+  canvas = p5canvas;
   movement = 0;
   angleMode(DEGREES);
+  // capturer.start();
 }
   
+
+
 function draw() {
+  // if (frameCount === 0) {
+  //   capturer.start();
+  // };
+
   background(220);
   translate(300, 300);
 
@@ -14,7 +42,38 @@ function draw() {
   strokeWeight(2);
   circle(0, 0, 300);
 
-  // strokeWeight(2);
+  time = time + 0.3;
+  stroke(0);
+  strokeWeight(5);
+  let move = map(time, 0, 60, 0, 360)
+  push();
+  rotate(move);
+  strokeWeight(4);
+  fill(0, 0, 255); 
+  ellipse(150, 0, 25, 25);
+  pop();
+
+
+  // if (frameCount < gifLength) {
+  //   capturer.capture(canvas);
+  // } else if (frameCount === gifLength) {
+  //   capturer.stop();
+  //   capturer.save();
+  // }
+
+  // capturer.capture(canvas);
+  
+  // let secondsElapsed = frameCount / framerate; 
+  // if (secondsElapsed >= 5) { 
+  //   capturer.save();
+  //   noLoop();}
+
+  
+}
+
+
+
+// strokeWeight(2);
   // line(0, 0, 450, 300);
 
   // strokeWeight(10);
@@ -45,26 +104,9 @@ function draw() {
   // 150 --> 0        0 --> -150        +-
   // 0 --> -150       -150 --> 0        --
   // -150 --> 0       0 --> 150         -+
-  
+
   // ellipse(x, y, 25, 25);
   // // if(x > 0 && x < 150 && y > 0 && y < 150) {
   //   x = x +1;
   //   y = y +1;
   // };
-  
-  time = time + 0.3;
-  stroke(0);
-  strokeWeight(5);
-  let move = map(time, 0, 60, 0, 360)
-  push();
-  rotate(move);
-  strokeWeight(4);
-  fill(0, 0, 255); 
-  ellipse(150, 0, 25, 25);
-  pop();
-
-
-
-
-}
-
